08fceb0c7c8d59244835ec82f390d78a6e1f1596
apanic <496043045qq.com>
2023-04-27 18:13:22 +0800

    init

---
+++ mixis/area.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		
+		watch: {
+			
+		},
+		
+		
+		
+		data() {
+			return {
+				province: 0,
+				city: 0,
+				area: 0,
+				
+				
+				provinceList: []
+			}
+		},
+		
+		mounted() {
+			this.getProvince()
+		},
+		
+		methods: {
+			getProvince() {
+				this.$request('/content/area/selectProvince',{},'GET').then(res => {
+					this.provinceList = res.data
+					console.log(this.provinceList)
+				})
+			}
+			
+		}
+
+	}
+</script>
+
+<style>
+</style>
